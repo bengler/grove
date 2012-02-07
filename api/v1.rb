35c054cc701bb50a7566bc3f7a39530cf34b3189
Katrina Owen <katrina.owen@gmail.com>
2012-02-07 14:56:25 +0100

    Add pingable

--- api/v1.rb
+++ api/v1.rb
@@ -9,29 +9,4 @@ class GroveV1 < Sinatra::Base
   set :root, "#{File.dirname(__FILE__)}/v1"
 
   register Sinatra::Pebblebed
-  i_am :grove
-
-
-  get '/ping' do
-    failures = []
-
-    begin
-      ActiveRecord::Base.verify_active_connections!
-      ActiveRecord::Base.connection.execute("select 1")
-    rescue Exception => e
-      failures << "ActiveRecord: #{e.message}"
-    end
-
-    begin
-      $memcached.get('ping')
-    rescue Exception => e
-      failures << "Memcached: #{e.message}"
-    end
-
-    if failures.empty?
-      halt 200, "grove"
-    else
-      halt 503, failures.join("\n")
-    end
-  end
 end
