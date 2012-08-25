93cbe736a0964103ec2aec2f14621e9c5a5a84a5
Katrina Owen <katrina.owen@gmail.com>
2012-08-25 16:21:45 +0200

    Refactor specs

--- spec/lib/models/posts_spec.rb
+++ spec/lib/models/posts_spec.rb
@@ -3,181 +3,179 @@ require 'spec_helper'
 describe Post do
 
   context "from uid" do
-    let(:card) { Post.create!(:uid => "post.card:area51.vaktmesterkontoret.forum1") }
+    let(:card) { Post.create!(:uid => "post.card:area51.ufo.pix") }
     subject { card }
 
     its(:realm) { should eq('area51') }
-    its(:canonical_path) { should eq('area51.vaktmesterkontoret.forum1') }
+    its(:canonical_path) { should eq('area51.ufo.pix') }
     its(:klass) { should eq('post.card') }
   end
 
   let(:default_attributes) do
     {
       :klass => 'post.doc',
-      :canonical_path => "area51.vaktmesterkontoret.forum1",
+      :canonical_path => "area51.secret.research",
       :document => {'text' => '1'}
     }
   end
 
-  let(:post) { Post.create!(default_attributes) }
+  let(:article) { Post.create!(default_attributes) }
 
-  subject { post }
+  subject { article }
 
   its(:realm) { should eq('area51') }
-  its(:uid) { should eq("post.doc:area51.vaktmesterkontoret.forum1$#{post.id}") }
+  its(:uid) { should eq("post.doc:area51.secret.research$#{article.id}") }
 
   context "locations" do
 
     it "defaults to the canonical location" do
-      subject.locations.map { |loc| loc.path.to_s }.should eq(["area51.vaktmesterkontoret.forum1"])
+      subject.locations.map { |loc| loc.path.to_s }.should eq(["area51.secret.research"])
     end
 
     it "cannot delete the canonical path" do
-      Location.declare!("x.y.z").posts << post
-      ->{ post.remove_path!('area51.vaktmesterkontoret.forum1') }.should raise_error ArgumentError
-      post.reload
-      post.paths.to_a.sort.should eq(['area51.vaktmesterkontoret.forum1', 'x.y.z'])
+      Location.declare!("area51.xyz").posts << article
+      ->{ article.remove_path!('area51.secret.research') }.should raise_error ArgumentError
+      article.reload
+      article.paths.to_a.sort.should eq(['area51.secret.research', 'area51.xyz'])
     end
 
     specify "are like symlinks" do
-      symlink = "area51.vikarkontoret.forum1"
-      Location.declare!(symlink).posts << post
+      symlink = "area51.classified.research"
+      Location.declare!(symlink).posts << article
 
-      Post.find_by_uid("post.doc:#{symlink}$#{post.id}").should eq(post)
+      Post.find_by_uid("post.doc:#{symlink}$#{article.id}").should eq(article)
     end
 
     specify "are not returned in duplicate" do
-      symlink = "area51.vikarkontoret.forum1"
-      Location.declare!(symlink).posts << post
+      symlink = "area51.classified.research"
+      Location.declare!(symlink).posts << article
 
-      Post.by_uid("post.doc:*").should eq([post])
+      Post.by_uid("post.doc:*").should eq([article])
     end
   end
 
   it 'atomically adds a path' do
-    post.should_not_receive(:save)
-    post.should_not_receive(:save!)
+    article.should_not_receive(:save)
+    article.should_not_receive(:save!)
 
-    post.add_path!("x.y.z")
+    article.add_path!("area51.xyz")
 
-    post.reload
-    post.paths.to_a.sort.should eq(['area51.vaktmesterkontoret.forum1', 'x.y.z'])
+    article.reload
+    article.paths.to_a.sort.should eq(['area51.secret.research', 'area51.xyz'])
   end
 
   it 'atomically deletes a path' do
-    other_post = Post.create!(:uid => "post:a.b.c.d.e", :document => {"text" => "2"})
-    Location.declare!("x.y.z").posts << post
-    Location.declare!("x.y.z").posts << other_post
+    other_article = Post.create!(:uid => "post:area51.abc", :document => {"text" => "2"})
+    Location.declare!("area51.xyz").posts << article
+    Location.declare!("area51.xyz").posts << other_article
 
-    post.should_not_receive(:save)
-    post.should_not_receive(:save!)
+    article.should_not_receive(:save)
+    article.should_not_receive(:save!)
 
-    post.remove_path!("x.y.z")
+    article.remove_path!("area51.xyz")
 
-    post.reload
-    other_post.reload
+    article.reload
+    other_article.reload
 
-    post.paths.to_a.should eq(["area51.vaktmesterkontoret.forum1"])
-    other_post.paths.to_a.sort.should eq(["a.b.c.d.e", "x.y.z"])
+    article.paths.to_a.should eq(["area51.secret.research"])
+    other_article.paths.to_a.sort.should eq(["area51.abc", "area51.xyz"])
   end
 
   describe "finders and filters" do
 
     it "finds by uid" do
-      Post.find_by_uid(post.uid).document['text'].should eq('1')
+      Post.find_by_uid(article.uid).document['text'].should eq('1')
     end
 
     it "returns nil for non-existant posts" do
-      Post.find_by_uid("post:area51.vaktmesterkontoret.forumX$2").should be_nil
+      Post.find_by_uid("post:area51.secret.work$2").should be_nil
     end
 
     it "filters by realm" do
+      # Does not match
       Post.create!(:uid => "post:oz.other.place")
 
-      posts = Post.filtered_by('realm' => post.realm)
-      posts.size.should eq(1)
-      posts.first.uid.should eq(post.uid)
+      Post.filtered_by('realm' => article.realm).should eq([article])
     end
 
     context "filters tags" do
       before(:each) do
-        Post.create!(:uid => "post:x.y.z", :tags => ["france", "paris"], :document => {'text' => '1'})
-        Post.create!(:uid => "post:x.y.z", :tags => ["capitals", "paris"], :document => {'text' => '2'})
-        Post.create!(:uid => "post:x.y.z", :tags => ["france", "lyon"], :document => {'text' => '3'})
+        Post.create!(:uid => "post.card:x.y.z", :tags => ["paris", "france"], :document => {'text' => '1'})
+        Post.create!(:uid => "post.card:x.y.z", :tags => ["paris", "texas"], :document => {'text' => '2'})
+        Post.create!(:uid => "post.card:x.y.z", :tags => ["lyon", "france"], :document => {'text' => '3'})
       end
 
-      it "finds only posts matching tag" do
+      specify "exclusively" do
         Post.with_tags("paris").all.map {|p| p.document['text']}.sort.should eq ['1', '2']
       end
 
-      it "finds only posts matching ALL tags" do
-        Post.with_tags(["france", "paris"]).all.map{|p| p.document['text']}.sort.should eq ['1']
+      it "with an exclusive AND" do
+        Post.with_tags(["paris", "france"]).all.map{|p| p.document['text']}.sort.should eq ['1']
       end
     end
 
-    describe "wildcard uids" do
+    describe "wildcard matches" do
       before(:each) do
-        Post.create!(:uid => "post:area51.vaktemsterkontoret.forum1", :document => {:text => "1"})
-        Post.create!(:uid => "post:area51.vaktemsterkontoret.forum1", :document => {:text => "2"})
-        Post.create!(:uid => "post:area51.vaktemsterkontoret.forum2", :document => {:text => "3"})
-        Post.create!(:uid => "post:area52.vaktemsterkontoret.forum2", :document => {:text => "4"})
+        Post.create!(:uid => "post:area51.secret.research", :document => {:text => "1"})
+        Post.create!(:uid => "post:area51.secret.research", :document => {:text => "2"})
+        Post.create!(:uid => "post:area51.secret.experiments", :document => {:text => "3"})
+        Post.create!(:uid => "post:area52.secret.experiments", :document => {:text => "4"})
       end
 
-      it "finds on wildcard with klass" do
+      specify "finds on klass" do
         Post.by_uid("post:*").map(&:document).map{|document| document[:text]}.sort.should eq ['1', '2', '3', '4']
       end
 
-      it "finds on wildcard with partial path" do
+      specify "finds on partial path" do
         Post.by_uid("post:area51.*").map(&:document).map{|document| document[:text]}.sort.should eq ['1', '2', '3']
       end
 
-      it "finds on wildcard with fully specified path" do
-        Post.by_uid("post:area51.vaktemsterkontoret.forum1").map(&:document).map{|document| document[:text]}.sort.should eq ['1', '2']
+      specify "finds on fully specified path" do
+        Post.by_uid("post:area51.secret.research").map(&:document).map{|document| document[:text]}.sort.should eq ['1', '2']
       end
     end
 
-    it "returns an empty set if there are no matches" do
-      Post.by_uid("post:highway61.*").should eq([])
+    specify "returns an empty set for wildcard uids with no matches" do
+      Post.by_uid("post:route66.*").should eq([])
     end
 
     describe "readthrough cache" do
-      let(:doc1) { post }
-      let(:doc2) { Post.create!(:uid => "post.doc:area51.vaktemsterkontoret.forum1", :document => {:text => "2"}) }
+      let(:memo) { Post.create!(:uid => "post.doc:area51.secret.research", :document => {:text => "2"}) }
 
       it "gets primed on fetch" do
-        Post.cached_find_all_by_uid([doc1.uid])
+        Post.cached_find_all_by_uid([article.uid])
 
-        post = JSON.parse($memcached.get(doc1.cache_key))
+        post = JSON.parse($memcached.get(article.cache_key))
         post['document']['text'].should eq '1'
       end
 
       it "reads from the cache" do
-        doc1.document = 'sentinel'
-        $memcached.set(doc1.cache_key, doc1.attributes.to_json)
+        article.document = 'sentinel'
+        $memcached.set(article.cache_key, article.attributes.to_json)
 
-        posts = Post.cached_find_all_by_uid([doc1.uid])
+        posts = Post.cached_find_all_by_uid([article.uid])
         posts.first.document.should eq 'sentinel'
       end
 
       it "respects order in the request" do
-        $memcached.set(doc1.cache_key, doc1.attributes.to_json)
+        $memcached.set(article.cache_key, article.attributes.to_json)
 
-        posts = Post.cached_find_all_by_uid([doc2.uid, doc1.uid])
-        posts.map {|post| post.document['text'] }.should eq(['2', '1'])
+        posts = Post.cached_find_all_by_uid([memo.uid, article.uid])
+        posts.map {|p| p.document['text'] }.should eq(['2', '1'])
       end
 
       it "performs with partial hits" do
-        $memcached.set(doc2.cache_key, doc2.attributes.to_json)
+        $memcached.set(memo.cache_key, memo.attributes.to_json)
 
-        posts = Post.cached_find_all_by_uid([doc1.uid, doc2.uid])
-        posts.map{|p| p.document['text']}.should eq(['1', '2'])
+        posts = Post.cached_find_all_by_uid([article.uid, memo.uid])
+        posts.map {|p| p.document['text']}.should eq(['1', '2'])
       end
 
       it "invalidates the cache" do
-        $memcached.set(doc1.cache_key, doc1.attributes.to_json)
-        doc1.document = "watchdog"
-        doc1.save!
-        posts = Post.cached_find_all_by_uid([doc1.uid])
+        $memcached.set(article.cache_key, article.attributes.to_json)
+        article.document = "watchdog"
+        article.save!
+        posts = Post.cached_find_all_by_uid([article.uid])
         posts.first.document.should eq 'watchdog'
       end
 
@@ -198,37 +196,50 @@ describe Post do
 
   context "restricted documents" do
 
-    before(:each) do
-      default_attributes.merge!(:restricted => true, :created_by => 42)
-    end
-
     let(:nobody) { DeepStruct.wrap({}) }
     let(:john_q_public) { DeepStruct.wrap({:id => 101, :god => false}) }
     let(:alice) { DeepStruct.wrap({:id => 42, :god => false}) }
     let(:zeus) { DeepStruct.wrap({:id => 1337, :god => true}) }
 
+    before(:each) do
+      default_attributes.merge!(:restricted => true, :created_by => 42)
+    end
+
     specify "are inaccessible without an identity" do
-      post.visible_to?(nobody).should eq false
+      article.visible_to?(nobody).should eq false
       Post.with_restrictions(nobody).size.should eq 0
     end
 
     specify "are inaccessible to random people" do
-      post.visible_to?(john_q_public).should eq false
+      article.visible_to?(john_q_public).should eq false
       Post.with_restrictions(john_q_public).size.should eq 0
     end
 
     specify "are accessible to document creator" do
-      post.visible_to?(alice).should eq true
+      article.visible_to?(alice).should eq true
       Post.with_restrictions(alice).size.should eq 1
     end
 
     specify "are accessible to god" do
-      post.visible_to?(zeus).should eq true
+      article.visible_to?(zeus).should eq true
       Post.with_restrictions(zeus).size.should eq 1
     end
   end
 
   describe "conflicts" do
+
+    # The document store in post consists of
+    # :external_document
+    # :document
+    # :merged_document (read-only)
+    #
+    # The external document is only relevant if the post is
+    # imported from or synced with an external source.
+    #
+    # The document will in this case be used to override the
+    # values of selected keys.
+    #
+    # The merged document is the combination of the two.
 
     let(:external_document) do
       {'title' => 'Greeting', 'text' => 'Hello, World!'}
@@ -245,7 +256,7 @@ describe Post do
     let(:external_post) { Post.create!(external_attributes) }
 
     it "isn't conflicted without an external document" do
-      post.conflicted?.should == false
+      article.conflicted?.should == false
     end
 
     it "isn't conflicted without a document" do
