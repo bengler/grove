8eb452595f8cb51756d26858d854b563af840490
Katrina Owen <katrina.owen@gmail.com>
2012-09-12 12:34:30 +0200

    Move location declaration into pebble_path gem

--- lib/models/location.rb
+++ lib/models/location.rb
@@ -2,11 +2,4 @@ class Location < ActiveRecord::Base
   include PebblePath
 
   has_and_belongs_to_many :posts, :uniq => true
-
-  def self.declare!(path)
-    raise ArgumentError, "Path must be valid" unless Pebblebed::Uid.valid_path?(path)
-    attributes = PebblePath.to_conditions(path)
-    path = self.where(attributes).first
-    path ||= self.create!(attributes)
-  end
 end
