bfbdaa9c9649acbae7fb99ca0367d82d0fc96e6d
Simen Svale Skogsrud <simen@bengler.no>
2012-09-30 19:28:48 +0200

    Eliminated http_origin check from rack-protection. It is handled by pebbles-cors

--- api/v1.rb
+++ api/v1.rb
@@ -9,6 +9,7 @@ class UnauthorizedChangeError < Exception; end
 
 class GroveV1 < Sinatra::Base
   set :root, "#{File.dirname(__FILE__)}/v1"
+  set :protection, :except => :http_origin
 
   register Sinatra::Pebblebed
 
