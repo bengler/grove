4e456d69150e3772a2888c93ee6ddfdf0c129b5f
Katrina Owen <katrina.owen@gmail.com>
2012-10-12 16:29:05 +0200

    Upgrade pebbles-uid and pebble_path

--- api/v1/readmarks.rb
+++ api/v1/readmarks.rb
@@ -11,7 +11,7 @@ class GroveV1 < Sinatra::Base
     identity_id = current_identity.try(:id)
     halt 403, "No identity" unless identity_id
 
-    if Pebbles::Uid::Path.new(path).wildcard?
+    if Pebbles::Uid::Labels.new(path).wildcard?
       pg :readmarks, :locals => { :readmarks => Readmark.where("owner = ?", identity_id).by_path(path) }
     else
       # This is a fully constrained path - return exactly one readmark
