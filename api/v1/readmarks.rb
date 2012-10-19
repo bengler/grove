03b2493898c03f36c1a56e06e4973b445d4d03d7
Katrina Owen <katrina.owen@gmail.com>
2012-10-19 15:59:04 +0200

    Refactor readmark api endpoints

--- api/v1/readmarks.rb
+++ api/v1/readmarks.rb
@@ -1,21 +1,20 @@
 class GroveV1 < Sinatra::Base
   put "/readmarks/:path/:uid" do |path, uid|
-    identity_id = current_identity.try(:id)
-    halt 403, "No identity" unless identity_id
+    require_identity
+
     oid = Pebbles::Uid.oid(uid)
-    readmark = Readmark.set!(identity_id, path, oid.to_i)
+    readmark = Readmark.set!(current_identity.id, path, oid.to_i)
     pg :readmark, :locals => { :readmark => readmark }
   end
 
   get "/readmarks/:path" do |path|
-    identity_id = current_identity.try(:id)
-    halt 403, "No identity" unless identity_id
+    require_identity
 
     if Pebbles::Uid::Labels.new(path).wildcard?
-      pg :readmarks, :locals => { :readmarks => Readmark.where("owner = ?", identity_id).by_path(path) }
+      pg :readmarks, :locals => { :readmarks => Readmark.where("owner = ?", current_identity.id).by_path(path) }
     else
       # This is a fully constrained path - return exactly one readmark
-      readmark = Readmark.where("owner = ?", identity_id).by_path(path).first
+      readmark = Readmark.where("owner = ?", current_identity.id).by_path(path).first
       halt 404, "No readmark for this path" unless readmark
       pg :readmark, :locals => { :readmark => readmark }
     end
