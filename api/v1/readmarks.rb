b81be455dc44a37e261b49cea693e7a74e90c078
Simen Svale Skogsrud <simen@bengler.no>
2012-11-14 14:53:33 +0100

    Removed dox for readmarks

--- api/v1/readmarks.rb
+++ api/v1/readmarks.rb
@@ -1,15 +1,6 @@
 class GroveV1 < Sinatra::Base
 
-  # @apidoc
-  # Set a readmark
-  #
-  # @category Grove/Readmarks
-  # @path /api/grove/v1/readmarks/:path/:uid
-  # @http PUT
-  # @example /api/grove/v1/posts/acme.blog/post.blog:acme.blog$123
-  # @required [String] path The path the user is currently reading
-  # @required [String] uid The uid of the last read post
-  # @status 200 Ok
+  # This api is extremely deprecated. Do not use!!
 
   put "/readmarks/:path/:uid" do |path, uid|
     require_identity
@@ -18,18 +9,6 @@ class GroveV1 < Sinatra::Base
     readmark = Readmark.set!(current_identity.id, path, oid.to_i)
     pg :readmark, :locals => { :readmark => readmark }
   end
-
-  # @apidoc
-  # Get readmarks
-  #
-  # @category Grove/Readmarks
-  # @path /api/grove/v1/readmarks/:path
-  # @http GET
-  # @example /api/grove/v1/posts/acme.*
-  # @required [String] path The path we want to check unread counts for. May be a wildcard path to retrieve
-  #   a collection of readmarks.
-  # @status 200 Ok
-  # @status 404 No readmarks for this path
 
   get "/readmarks/:path" do |path|
     require_identity
