9d0f804ae37596d10f400566dce12d69161a829f
Alexander Staubo <alex@bengler.no>
2013-04-05 19:21:47 +0200

    Fix failing specs due to LOGGER not being defined.

--- config/logging.rb
+++ config/logging.rb
@@ -1,6 +1,7 @@
 require "logger"
 
-Log = LOGGER if defined?(LOGGER)
-Log ||= Logger.new(STDOUT)
+unless defined?(LOGGER)
+  LOGGER = Logger.new("/dev/null")
+end
 
 ActiveRecord::Base.logger.level = Logger::WARN if ActiveRecord::Base.logger
